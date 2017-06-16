938e124791327632985b78aab4628a542e4b6eb4
Jennilyn Howell <jennilynhowell@me.com>
2017-06-16 15:55:08 -0400

    change styling to bootstrap

--- js/main.js
+++ js/main.js
@@ -8,43 +8,59 @@
   let headers = {};
   let urlUser = 'https://api.github.com/users/jennilynhowell';
   let urlRepos = 'https://api.github.com/users/jennilynhowell/repos';
+  let urlOrgs = 'https://api.github.com/user/orgs';
 
   let mainNode = document.getElementById('mainSection');
   let sidebarNode = document.createElement('aside');
   let repoNode = document.createElement('section');
-  sidebarNode.setAttribute('class', 'sidebar');
-  repoNode.setAttribute('class', 'repoSection');
+  sidebarNode.setAttribute('class', 'sidebar row-fluid col-md-3');
+  repoNode.setAttribute('class', 'repoSection col-md-9');
   mainNode.appendChild(sidebarNode);
   mainNode.appendChild(repoNode);
 
 
-  // if (GIT_TOKEN) {
-    //set AJAX header to send token
-    try {
-    headers['Authorization'] = 'token ' + GIT_TOKEN;
-  } catch (e) {
-    //ignore error
-  }
+  if (GIT_TOKEN) {
+      // set AJAX header to send token
+      try {
+      headers['Authorization'] = 'token ' + GIT_TOKEN;
+    } catch (e) {
+      //ignore error
+    }
+
+    // fetch(urlOrgs, {headers: headers}).then(function(response) {
+    //   response.json().then(function(data){
+    //       console.log(data);
+    //
+    //       let orgs = '';
+    //     })
+    //
+    //   })
+    // }
 
     fetch(urlUser, {headers: headers}).then(function(response) {
       response.json().then(function(data){
-        console.log(data);
+          console.log(data);
 
+          //sidebar
           let name = data.name;
-          console.log(name);
           let bio = data.bio;
-          console.log(bio);
           let loc = data.location;
-          console.log(loc);
           let email = data.email;
-          console.log(email);
           let photo = data.avatar_url;
-          console.log(photo);
           let web = data.html_url;
-          console.log(web);
 
-        console.log(name, bio, loc, email, photo, web);
+          //subheader
+          let repos = data.repos_url;
+          let numRepos = data.public_repos;
+          let stars = data.starred_url;
+          let numFollowers = data.followers;
+          let followers = data.followers_url;
+          let numFollowing = data.following;
+          let following = data.following_url;
+
+
         buildSidebar(name, bio, loc, email, photo, web);
+        buildSubHeader(repos, numRepos, stars, numFollowers, followers, numFollowing, following);
       })
 
     })
@@ -56,46 +72,41 @@
 
         for (let i = 0; i < data.length; i++) {
           let repoName = data[i].name;
-          console.log(repoName);
           let repoLink = data[i].html_url;
-          console.log(repoLink);
           let repoDesc = data[i].description;
-          console.log(repoDesc);
           let repoLang = data[i].language;
             if (data[i].language === null){
               repoLang = 'language';
             };
-          console.log(repoLang);
           let repoUpdate = data[i].updated_at;
-          console.log(repoUpdate);
 
           buildRepoSection(repoName, repoLink, repoDesc, repoLang, repoUpdate);
         };
       })
     })
-  // }
+  }
 
   //Create HTML structure for sidebar
 
   function buildSidebar(name, bio, loc, email, photo, web) {
 
     let photoDiv = document.createElement('div');
-    photoDiv.setAttribute('id', 'photoDiv');
+    photoDiv.setAttribute('id', 'photoDiv col-md-3');
     sidebarNode.appendChild(photoDiv);
-    photoDiv.innerHTML = '<img src="' + photo + '">';
+    photoDiv.innerHTML = '<img class="img-rounded img-responsive" src="' + photo + '">';
 
     let nameDiv = document.createElement('h1');
-    nameDiv.setAttribute('id', 'nameDiv');
+    nameDiv.setAttribute('id', 'nameDiv col-md-3');
     sidebarNode.appendChild(nameDiv);
     nameDiv.textContent = name;
 
     let bioDiv = document.createElement('p');
-    bioDiv.setAttribute('id', 'bioDiv');
+    bioDiv.setAttribute('id', 'bioDiv col-md-3');
     sidebarNode.appendChild(bioDiv);
     bioDiv.textContent = bio;
 
     let locDiv = document.createElement('div');
-    locDiv.setAttribute('id', 'locDiv');
+    locDiv.setAttribute('id', 'locDiv col-md-3');
     let locIcon = document.createElement('span');
     let locText = document.createElement('p');
     locIcon.setAttribute('class', 'octicon octicon-location');
@@ -105,14 +116,77 @@
     locText.textContent = loc;
 
     let emailDiv = document.createElement('p');
-    emailDiv.setAttribute('id', 'emailDiv');
+    emailDiv.setAttribute('id', 'emailDiv col-md-4');
     sidebarNode.appendChild(emailDiv);
     emailDiv.textContent = email;
 
     let webDiv = document.createElement('div');
-    webDiv.setAttribute('id', 'webDiv');
+    webDiv.setAttribute('id', 'webDiv col-md-4');
     sidebarNode.appendChild(webDiv);
     webDiv.textContent = web;
+
+    let orgsDiv = document.createElement('div');
+    orgsDiv.setAttribute('id', 'orgsDiv col-md-4');
+    sidebarNode.appendChild(orgsDiv);
+    let orgsHead = document.createElement('h5');
+    let orgsLinks = document.createElement('span');
+    orgsDiv.appendChild(orgsHead);
+    orgsDiv.appendChild(orgsLinks);
+    orgsHead.textContent = 'Organizations';
+
+  }
+
+  //Create subheader above repos
+  function buildSubHeader(repos, numRepos, stars, numFollowers, followers, numFollowing, following){
+    let subHeader = document.createElement('div');
+    subHeader.setAttribute('class', 'subHeader');
+    repoNode.appendChild(subHeader);
+
+    let subheadList = document.createElement('ul');
+    subheadList.setAttribute('class', 'subheadList list-unstyled list-inline');
+    subHeader.appendChild(subheadList);
+    subheadList.innerHTML = `
+                            <li><a href="#">Overview</a></li>
+                            <li><a href="${repos}">Repositories</a><span>${numRepos}</span></li>
+                            <li><a href="${stars}">Stars</a></li>
+                            <li><a href="${followers}">Followers</a><span>${numFollowers}</span></li>
+                            <li><a href="${following}">Following</a><span>${numFollowing}</span></li>
+                            `;
+
+    let subheadForm = document.createElement('form');
+    subheadForm.setAttribute('class', 'subheadForm form-inline');
+    subHeader.appendChild(subheadForm);
+    let subheadSearchField = document.createElement('input');
+    subheadSearchField.setAttribute('type', 'search');
+    subheadSearchField.setAttribute('class', 'form-control');
+    subheadSearchField.setAttribute('id', 'subheadSearchField');
+    subheadSearchField.setAttribute('placeholder', 'Search repositories...');
+    subheadForm.appendChild(subheadSearchField);
+    let subheadSelectType = document.createElement('select');
+    subheadSelectType.setAttribute('class', 'subheadSelectType form-control');
+    subheadSelectType.setAttribute('class', 'form-control');
+    subheadForm.appendChild(subheadSelectType);
+    subheadSelectType.innerHTML = `
+                                  <option value='all'>Type: All</option>
+                                  <option value='public'>Type: Public</option>
+                                  <option value='private'>Type: Private</option>
+                                  <option value='sources'>Type: Sources</option>
+                                  <option value='forks'>Type: Forks</option>
+                                  <option value='mirrors'>Type: Mirrors</option>
+                                  `;
+    let subheadSelectLang = document.createElement('select');
+    subheadSelectLang.setAttribute('class', 'subheadSelectLang form-control');
+    subheadForm.appendChild(subheadSelectLang);
+    subheadSelectLang.innerHTML = `
+                                  <option value='all'>Language: All</option>
+                                  <option value='html'>Language: HTML</option>
+                                  <option value='css'>Language: CSS</option>
+                                  <option value='js'>Language: JavaScript</option>
+                                  `;
+    let newRepoBtn = document.createElement('button');
+    newRepoBtn.setAttribute('class', 'newRepoBtn octicon octicon-repo btn btn-success form-control');
+    subheadForm.appendChild(newRepoBtn);
+    newRepoBtn.textContent = ' New';
 
   }
 
@@ -123,7 +197,7 @@
     repoBox.setAttribute('class', 'repoBox');
     repoNode.appendChild(repoBox);
 
-    let repoTitle = document.createElement('h1');
+    let repoTitle = document.createElement('h3');
     repoTitle.setAttribute('class', 'repoTitle');
     repoTitle.innerHTML = '<a href="' + link + '">' + name + '</a>';
     repoBox.appendChild(repoTitle);
@@ -145,6 +219,9 @@
     repoBox.appendChild(repoUpdate);
 
   }
+
+
+
 
 
 
